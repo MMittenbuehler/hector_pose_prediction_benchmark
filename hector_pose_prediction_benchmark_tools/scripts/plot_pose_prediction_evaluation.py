976fbfd9ea06125ffe520db28d4b29843fa71918
Martin Oehler <oehler@sim.tu-darmstadt.de>
2023-08-18 15:14:34 +0200

    Added angular error

--- hector_pose_prediction_benchmark_tools/scripts/plot_pose_prediction_evaluation.py
+++ hector_pose_prediction_benchmark_tools/scripts/plot_pose_prediction_evaluation.py
@@ -3,7 +3,7 @@ import plotly.graph_objects as go
 import numpy as np
 import os
 import argparse
-
+from pytransform3d import rotations as pr
 import plotly.io as pio
 pio.kaleido.scope.mathjax = None
 
@@ -39,15 +39,40 @@ def plot_comparison(data, labels: list):
     fig.write_image("images/" + "_".join(labels) + ".pdf")
     #fig.show()
 
-def compute_metrics(data, label1, label2):
+def compute_label_metrics(data, label1, label2):
     diff = np.abs(data[label1] - data[label2])
-    mean = np.mean(diff)
+
     print(f"{label1} - {label2}")
-    if "roll" in label1 or "pitch" in label1:
-        print("mean: ", mean, "deg:", np.rad2deg(mean))
-    else:
-        print("mean:", mean)
+    angle = "roll" in label1 or "pitch"
+    compute_metrics(diff, angle)
     print("")
+
+def compute_angular_error(data, label1_prefix, label2_prefix):
+    pose_suffixes = ["orientation_roll", "orientation_pitch", "orientation_yaw"]
+    label1 = [label1_prefix + "_" + suffix for suffix in pose_suffixes]
+    label2 = [label2_prefix + "_" + suffix for suffix in pose_suffixes]
+
+    angles = np.zeros(data.size)
+    for i, row in enumerate(data[label1 + label2]):
+        q1 = pr.quaternion_from_euler(row[label1], 0, 1, 2, True)
+        q2 = pr.quaternion_from_euler(row[label2], 0, 1, 2, True)
+        angle_axis = pr.quaternion_diff(q1, q2)
+        angles[i] = angle_axis[3]
+
+    print(f"{label1_prefix} - {label2_prefix}: rotation error")
+    compute_metrics(angles, True)
+    print("")
+
+def compute_metrics(data, angle=False):
+    mean = np.mean(data)
+    std = np.std(data)
+    maximum = np.max(np.abs(data))
+    if angle:
+        print(f"mean: {mean:0.4f} +/- {std}, deg: {np.rad2deg(mean):0.4f} +/- {np.rad2deg(std):0.4f}", )
+        print(f"max: {maximum}, deg: {np.rad2deg(maximum)}")
+    else:
+        print(f"mean: {mean:0.4f} +/- {std:0.4f}")
+        print(f"max: {maximum:0.4f}, deg: {np.rad2deg(maximum):0.4f}")
 
 
 def main():
@@ -64,10 +89,12 @@ def main():
     plot_comparison(data, position_z_comparison_labels)
     plot_comparison(data, roll_comparison_labels + pitch_comparison_labels)
 
-    compute_metrics(data, *stability_comparison_labels)
-    compute_metrics(data, *position_z_comparison_labels)
-    compute_metrics(data, *roll_comparison_labels)
-    compute_metrics(data, *pitch_comparison_labels)
+    compute_label_metrics(data, *stability_comparison_labels)
+    compute_label_metrics(data, *position_z_comparison_labels)
+    compute_label_metrics(data, *roll_comparison_labels)
+    compute_label_metrics(data, *pitch_comparison_labels)
+
+    compute_angular_error(data, "ground_truth", "predicted")
 
 
 if __name__ == '__main__':
